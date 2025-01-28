d9c48b5cd1a8de3b7b3c8c5cf18031a2bd9db230
CHarvey820 <charvey820@gmail.com>
2025-01-27 22:44:17 -0600

    commit - reupload to fix file structure for pages

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const shareIconBtns = document.querySelectorAll(".iconArrow");
+const socials_shareActive = document.querySelector(".socials_shareActive");
+const iconArrowSocialsActive = document.querySelector(".iconArrowSocialsActive");
+const iconArrowSocialsInactive = document.querySelector(".iconArrowSocialsInactive");
+
+let mql = window.matchMedia("(min-width: 930px)");
+
+/**
+ * Adds a click event to all iconArrow elements to toggle the "isHidden" class of the socials_shareActive element
+ * @param {any} shareIconBtns
+ * @returns {any}
+ */
+Array.from(shareIconBtns).forEach((shareIconBtn) => {
+    shareIconBtn.addEventListener("click", () => {
+// Checks if the media query for desktop screens is active, in which case the socials_shareActive appears above. the existing arrow is used but changes color schemes
+        if (mql.matches) {
+            iconArrowSocialsActive.classList.add("isHidden");
+            socials_shareActive.classList.toggle("isHidden");
+            iconArrowSocialsInactive.classList.add("arrowDark");
+            //change the arrow back to its regular state when closing the socials on desktop
+            if(socials_shareActive.classList.contains("isHidden")){
+                iconArrowSocialsInactive.classList.remove("arrowDark");
+            }
+        } else {
+            socials_shareActive.classList.toggle("isHidden");
+        }
+    });
+});
+
+/**
+ * Handles arrow change if user resizes screen while socials are active
+ * @param {any} 'resize'
+ * @param {any} (
+ * @returns {any}
+ */
+window.addEventListener('resize', () => {
+    if (mql.matches) {
+        iconArrowSocialsActive.classList.add("isHidden");
+        if (!socials_shareActive.classList.contains("isHidden")){
+            iconArrowSocialsInactive.classList.add("arrowDark");
+        }
+    } else {
+        iconArrowSocialsActive.classList.remove("isHidden");
+        iconArrowSocialsInactive.classList.remove("arrowDark");
+    }  
+});
